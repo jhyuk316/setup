b9d13827e2a780f5353fc5c378b3fdfbf2b8e8e5
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-01-22 03:09:32 +0900

    스크립트 수정

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
+# brew
+echo "Install Brew"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# oh my zsh
+echo "Install oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# p10k
+echo "Install powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
